44cfa55ca06da6f306481f89a805efdf4f67b4ba
James Cunningham <james@bauscode.com>
2016-09-29 18:31:11 -0600

    chore(BAUS) add product images

--- BausCode.Api.Models/ProductImage.cs
+++ BausCode.Api.Models/ProductImage.cs
@@ -1,35 +1,24 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using ServiceStack;
 using ServiceStack.DataAnnotations;
 
 namespace BausCode.Api.Models
 {
-    public class Product : IAuditable
+    public class ProductImage : IAuditable
     {
-        public Product()
-        {
-            Meta = new ProductMeta();
-        }
-
         [PrimaryKey]
         [AutoIncrement]
         public int Id { get; set; }
 
         public long ShopifyId { get; set; }
 
-        public ProductMeta Meta { get; set; }
+        [ForeignKey(typeof (Product), OnDelete = "CASCADE", OnUpdate = "CASCADE")]
+        public int ProductId { get; set; }
 
         public DateTime CreateDate { get; set; }
         public DateTime ModifyDate { get; set; }
         public ulong RowVersion { get; set; }
 
-        [Reference]
-        public List<ProductVariant> Variants { get; set; }
-
-        [Reference]
-        public List<ProductImage>  Images { get; set; }
+        public string SourceUrl { get; set; }
 
         public void OnInsert()
         {

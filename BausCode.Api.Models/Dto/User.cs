d5bc13f2e232b853dfe89ee57e3728240c0952da
James Cunningham <james@bauscode.com>
2016-10-01 09:28:12 -0600

    chore(DERP) add basic user listing endpoint, stub user search

---
+++ BausCode.Api.Models/Dto/User.cs
@@ -0,0 +1,19 @@
+﻿using ServiceStack.Auth;
+
+namespace BausCode.Api.Models.Dto
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Email { get; set; }
+
+        public static User From(UserAuth source)
+        {
+            return new User()
+            {
+                Id = source.Id,
+                Email = source.Email
+            };
+        }
+    }
+}
---
+++ BausCode.Api.Models/Routing/SearchUsers.cs
@@ -0,0 +1,10 @@
+﻿using ServiceStack;
+
+namespace BausCode.Api.Models.Routing
+{
+    [Route("/api/v1/users/search", "POST")]
+    public class SearchUsers : IReturn<SearchUsersResponse>
+    {
+        public string Query { get; set; }
+    }
+}

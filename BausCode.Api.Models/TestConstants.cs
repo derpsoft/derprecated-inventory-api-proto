6d9cd4afd5214af23ce283755d787e67be3c1e3f
James Cunningham <james@bauscode.com>
2016-11-15 18:26:27 -0700

    add(DERP) base testing dependencies

---
+++ BausCode.Api.Models/TestConstants.cs
@@ -0,0 +1,11 @@
+﻿namespace BausCode.Api.Models
+{
+    public static class TestConstants
+    {
+        public static class Authors
+        {
+            public const string James = "James Cunningham <james@derprecated.com>";
+            public const string Allen = "Allen Tong <allen@derprecated.com>";
+        }
+    }
+}
---
+++ Derprecated.Api.Test/TestTests.cs
@@ -0,0 +1,15 @@
+﻿using BausCode.Api.Models;
+using NUnit.Framework;
+
+namespace Derprecated.Api.Test
+{
+    [TestFixture]
+    public class TestTests
+    {
+        [Test(Author = TestConstants.Authors.James)]
+        public void CanRunTests()
+        {
+            Assert.True(true);
+        }
+    }
+}

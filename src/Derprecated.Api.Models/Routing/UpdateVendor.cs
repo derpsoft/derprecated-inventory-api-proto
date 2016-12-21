7e4f1fe842c0725b5ee1d9d5155f98faad18403d
James Cunningham <james@bauscode.com>
2016-12-21 14:31:24 -0700

    feat(DERP) vendor update

---
+++ src/Derprecated.Api.Models/Routing/UpdateVendor.cs
@@ -0,0 +1,13 @@
+﻿namespace Derprecated.Api.Models.Routing
+{
+    using ServiceStack;
+
+    [Route("/api/v1/vendors/{Id}", "PUT, PATCH")]
+    [Authenticate]
+    [RequiresAnyPermission(Permissions.CanDoEverything, Permissions.CanManageVendors, Permissions.CanUpsertVendors)]
+    public class UpdateVendor : IReturn<VendorResponse>
+    {
+        public int Id { get; set; }
+        public Dto.Vendor Vendor { get; set; }
+    }
+}

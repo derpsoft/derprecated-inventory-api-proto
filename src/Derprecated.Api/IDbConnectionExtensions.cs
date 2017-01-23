dbe1ff56e435be18e0a8ab6a2ac827ad8418d216
James Cunningham <james@bauscode.com>
2017-01-23 13:00:08 -0700

    feat(DERP) add location soft deletes

---
+++ src/Derprecated.Api/IDbConnectionExtensions.cs
@@ -0,0 +1,19 @@
+﻿namespace Derprecated.Api
+{
+    using System;
+    using System.Data;
+    using Models;
+    using ServiceStack.OrmLite;
+
+    public static class DbConnectionExtensions
+    {
+        public static T SoftDelete<T>(this IDbConnection source, T record)
+            where T: ISoftDeletable
+        {
+            record.IsDeleted = true;
+            record.DeleteDate = DateTime.UtcNow;
+            source.Update(record);
+            return record;
+        }
+    }
+}
